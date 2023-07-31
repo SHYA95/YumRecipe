c6fdbde8c1035b00d581817201e4f5731cdeee95
SHYA95 <yassershrouk254@gmail.com>
2023-07-31 22:45:10 +0300

    Modify Login Screen

--- Scenes/LoginScreen/Controller/LoginViewController.swift
+++ Scenes/LoginScreen/Controller/LoginViewController.swift
@@ -4,26 +4,40 @@
 //
 //  Created by Shrouk Yasser on 29/07/2023.
 //
-
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var EmailTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    var viewModel: LoginViewModel! // Declare the view model
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupViewModel()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func setupViewModel() {
+        viewModel = LoginViewModel()
     }
-    */
-
+    
+    @IBAction func LoginButtonTapped(_ sender: UIButton) {
+        guard let email = EmailTextField.text, let password = PasswordTextField.text else {
+            return
+        }
+        
+        viewModel.login(email: email, password: password) { [weak self] success, error in
+            if success {
+                let homeViewController = HomeViewController()
+                let navigationController = UINavigationController(rootViewController: homeViewController)
+                navigationController.modalPresentationStyle = .fullScreen
+                self?.present(navigationController, animated: true, completion: nil)
+            } else {
+                self?.showToast(message: error)
+                self?.EmailTextField.showErrorBorder()
+                self?.PasswordTextField.showErrorBorder()
+            }
+        }
+    }
 }
--- Scenes/LoginScreen/Controller/LoginViewModel.swift
+++ Scenes/LoginScreen/Controller/LoginViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+class LoginViewModel {
+    func login(email: String, password: String, completion: @escaping (Bool, String) -> Void) {
+            // Check email format
+            guard isValidEmail(email) else {
+                completion(false, "Invalid email format")
+                return
+            }
+            
+            // Check email and password
+        if email == NetworkConstants.validEmail && password == NetworkConstants.validPassword {
+                completion(true, "")
+            } else {
+                completion(false, "Incorrect email or password")
+            }
+        }
+        
+        private func isValidEmail(_ email: String) -> Bool {
+            let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+            let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailRegex)
+            return emailPredicate.evaluate(with: email)
+        }
+    }
